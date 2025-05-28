9523c5e86bd7b18504d506b9738c071c1dd76471
Pankaj kori <93833439+pankajkori81@users.noreply.github.com>
2025-05-28 11:37:33 +0530

    Add files via upload

---
+++ src/Component/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Home from '../Home/Home'
+
+const Header = () => {
+  return (
+    <div id='Home'>
+
+      <Home/>
+    </div>
+  )
+}
+
+export default Header
+
